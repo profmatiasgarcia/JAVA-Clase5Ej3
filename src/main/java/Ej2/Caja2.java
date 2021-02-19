c766bc0064ee844c56d34ec628170518fbbe8c5e
Prof Matias Garcia <54009386+profmatiasgarcia@users.noreply.github.com>
2021-02-19 17:51:02 -0300

    Add files via upload

---
+++ src/main/java/Ej2/Caja2.java
@@ -0,0 +1,45 @@
+package Ej2;
+/**
+ * @author Prof Matias Garcia.
+ * <p> Copyright (C) 2017 para <a href = "https://www.profmatiasgarcia.com.ar/"> www.profmatiasgarcia.com.ar </a>
+ * - con licencia GNU GPL3.
+ * <p> Este programa es software libre. Puede redistribuirlo y/o modificarlo bajo los términos de la
+ * Licencia Pública General de GNU según es publicada por la Free Software Foundation, 
+ * bien con la versión 3 de dicha Licencia o bien (según su elección) con cualquier versión posterior. 
+ * Este programa se distribuye con la esperanza de que sea útil, pero SIN NINGUNA GARANTÍA, 
+ * incluso sin la garantía MERCANTIL implícita o sin garantizar la CONVENIENCIA PARA UN PROPÓSITO
+ * PARTICULAR. Véase la Licencia Pública General de GNU para más detalles.
+ * Debería haber recibido una copia de la Licencia Pública General junto con este programa. 
+ * Si no ha sido así ingrese a <a href = "http://www.gnu.org/licenses/"> GNU org </a>
+ */
+public class Caja2 {
+
+    private int valor;
+    private boolean disponible = false;
+
+    public synchronized void meter(int nv) {
+        if (disponible) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+        valor = nv;
+        disponible = true;
+        System.out.println("metido el valor: " + valor);
+        notify();
+    }
+
+    public synchronized void sacar() {
+        if (!disponible) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+        System.out.println("sacado el valor: " + valor);
+        valor = 0;
+        disponible = false;
+        notify();
+    }
+}
